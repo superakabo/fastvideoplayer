a434e2fac2d3a7568e6f381a8a092fde9ba91851
Samuel Akabo <superakabo@gmail.com>
2023-04-27 09:01:46 +0000

    Added a copyWith function to FastVideoPlayerStrings

--- lib/src/fast_video_player_strings.dart
+++ lib/src/fast_video_player_strings.dart
@@ -1,12 +1,43 @@
 class FastVideoPlayerStrings {
-  const FastVideoPlayerStrings();
+  final String play;
+  final String pause;
+  final String fullScreen;
+  final String exitFullScreen;
+  final String forward10Seconds;
+  final String replay10Seconds;
+  final String mute;
+  final String unmute;
 
-  final String play = 'Play';
-  final String pause = 'Pause';
-  final String fullScreen = 'Full screen';
-  final String exitFullScreen = 'Exit full screen';
-  final String forward10Seconds = 'Forward 10 seconds';
-  final String replay10Seconds = 'Replay 10 seconds';
-  final String mute = 'Mute';
-  final String unmute = 'Unmute';
+  const FastVideoPlayerStrings({
+    this.play = 'Play',
+    this.pause = 'Pause',
+    this.fullScreen = 'Mute',
+    this.exitFullScreen = 'Unmute',
+    this.forward10Seconds = 'Full screen',
+    this.replay10Seconds = 'Exit full screen',
+    this.mute = 'Forward 10 seconds',
+    this.unmute = 'Replay 10 seconds',
+  });
+
+  FastVideoPlayerStrings copyWith({
+    String? play,
+    String? pause,
+    String? fullScreen,
+    String? exitFullScreen,
+    String? forward10Seconds,
+    String? replay10Seconds,
+    String? mute,
+    String? unmute,
+  }) {
+    return FastVideoPlayerStrings(
+      play: play ?? this.play,
+      pause: pause ?? this.pause,
+      mute: mute ?? this.mute,
+      unmute: unmute ?? this.unmute,
+      fullScreen: fullScreen ?? this.fullScreen,
+      exitFullScreen: exitFullScreen ?? this.exitFullScreen,
+      forward10Seconds: forward10Seconds ?? this.forward10Seconds,
+      replay10Seconds: replay10Seconds ?? this.replay10Seconds,
+    );
+  }
 }
